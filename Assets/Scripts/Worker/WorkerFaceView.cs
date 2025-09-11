6f4939681d1ccef8b6a959b1a4af2e6f3786afa4
Shingo Tabuchi <tabuchishingo123@gmail.com>
2025-09-12 02:08:05 +0900

    a

--- Assets/Scripts/Worker/WorkerFaceView.cs
+++ Assets/Scripts/Worker/WorkerFaceView.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class WorkerFaceView : MonoBehaviour
 {
     [SerializeField] private SkinnedMeshRenderer _eyeMaskRendererL;
     [SerializeField] private SkinnedMeshRenderer _eyeFrameRendererL;
     [SerializeField] private SkinnedMeshRenderer _eyeMaskRendererR;
     [SerializeField] private SkinnedMeshRenderer _eyeFrameRendererR;
-    [SerializeField] private AnimationCurve _normalCurve;
 
     public float EyeOpenScale = 1.0f;
     [Range(0, 1)] public float EyeOpenAmount = 1.0f;
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     public void UpdateView(float deltaTime)
     {
@@ -20,8 +27,24 @@ public class WorkerFaceView : MonoBehaviour
         _eyeFrameRendererR.SetBlendShapeWeight(0, blink);
     }
 
-    // [Alchemy.Inspector.Button]
-    private void TestBlink()
+
+#if UNITY_EDITOR
+    [Fwk.Editor.Button]
+#endif
+    private void Blink()
     {
+        _animator.Play("Blink", 1, 0);
     }
+
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        if (Application.isPlaying)
+        {
+            return;
+        }
+
+        UpdateView(0.016f);
+    }
+#endif
 }
