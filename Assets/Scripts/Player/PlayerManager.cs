dba749ee9b08dc616b3d1c2f3f8abb17a2f8dbeb
Shingo Tabuchi <tabuchishingo123@gmail.com>
2025-09-15 19:00:47 +0900

    no message

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -2,13 +2,31 @@ using Fwk;
 
 public class PlayerManager : SingletonGeneric<PlayerManager>
 {
+    private bool _initialized;
+
     public void Initialize()
     {
+        if (_initialized)
+        {
+            return;
+        }
+
+        if (PlayerMover.Instance == null)
+        {
+            return;
+        }
+
         PlayerInputs.Instance.Initialize();
+        _initialized = true;
     }
 
     public void Update(float deltaTime)
     {
+        if (!_initialized)
+        {
+            return;
+        }
+
         PlayerInputs.Instance.Update(deltaTime);
         PlayerMover.Instance.UpdateMover(deltaTime);
     }
